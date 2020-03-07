376fb0683d9a178f14080e53b5489aeda92659e1
Valentin <40268974+valentinaubert@users.noreply.github.com>
2020-03-07 13:49:41 +0100

    Add files via upload

--- src/common/bddConnexion.java
+++ src/common/bddConnexion.java
@@ -1,9 +1,44 @@
 package common;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author p1810879
  */
 public class bddConnexion {
+      
+    private static String dbUrl = "";
+    private static String dbUser = " ";
+    private static String dbPassword = " ";
+    private static String query = " ";
+    private static Connection conn = null;
+    
+    public static Connection getConnection() throws SQLException
+    {       
+        if(conn != null)
+        {
+            return conn;
+        }else {
+            conn = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+        return conn;
+        }        
+    }
+    
+    public static ResultSet getRequest(String query) throws SQLException
+    {
+        Statement  stmt = null;
+        ResultSet rs = null;
+        stmt.getConnection().prepareStatement(query);
+        rs = stmt.executeQuery(query);
+        return rs;
+    }
+    
+    
     
 }
